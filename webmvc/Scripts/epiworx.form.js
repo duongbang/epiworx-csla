5f36e7f3a05c819761d5c796aeaf26989fbb4cc7
unknown <mattruma@gmail.com>
2011-07-03 07:03:43 -0400

    added jquery date picker

---
+++ webmvc/Scripts/epiworx.form.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+});
+
+function deleteRecord(action) {
+    if (confirm("Are you sure you want to delete this item?")) {
+        location.href = action;
+    }
+}
---
+++ webmvc/Scripts/epiworx.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+   
+    $("input.date").datepicker({
+        showButtonPanel: true,
+        prevText: "<<",
+        nextText: ">>"
+    });
+   
+    $("ul li:first-child").addClass("first");
+    $("ul li:last-child").addClass("last");
+
+});
